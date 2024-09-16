4ba7a7b45624b004d1f29d276b324802cb0897fe
suman492 patro <suman.pat49@gmail.com>
2024-09-16 09:52:13 +0000

    updated project

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const mongoose = require('mongoose');
+
+connectDB();
 const app = express();
 const port = 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/myDataBase', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {console.log("connected to mongodb successfully")});
+// mongoose.connect('mongodb://localhost/myDataBase', { useNewUrlParser: true, useUnifiedTopology: true });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {console.log("connected to mongodb successfully")});
 
 
 let books = [];
@@ -18,49 +21,49 @@ app.get('/books',(req,res) => {
 
 app.use(express.json())
 
-app.post('/books',(req,res) => {
-    const { title, author } = req.body;
-    if(!title || !author) {
-        return res.status(400).send('missing title or author');
-    }
+// app.post('/books',(req,res) => {
+//     const { title, author } = req.body;
+//     if(!title || !author) {
+//         return res.status(400).send('missing title or author');
+//     }
     
-    const newBook = { id: books.length + 1, title, author };
-    books.push(newBook);
-    res.status(201).send(newBook);
-});
-app.get('/books',(req,res) => {
-    res.json(books);
-});
-app.get('/books/:id',(req,res) => {
-    const book = books.find(b => b.id === parseInt(req.params.id));
-    if(!book) {
-        return res.status(404).send('book not found');
-    }
-    res.json(book);
+//     const newBook = { id: books.length + 1, title, author };
+//     books.push(newBook);
+//     res.status(201).send(newBook);
+// });
+// app.get('/books',(req,res) => {
+//     res.json(books);
+// });
+// app.get('/books/:id',(req,res) => {
+//     const book = books.find(b => b.id === parseInt(req.params.id));
+//     if(!book) {
+//         return res.status(404).send('book not found');
+//     }
+//     res.json(book);
     
-});
-app.put('/books/:id',(req,res) => {
-    const book = books.find(b => b.id === parseInt(req.params.id));
-    if(!book) {
-        return res.status(404).send('book not found');
-    }
+// });
+// app.put('/books/:id',(req,res) => {
+//     const book = books.find(b => b.id === parseInt(req.params.id));
+//     if(!book) {
+//         return res.status(404).send('book not found');
+//     }
 
-    const { title, author } = req.body;
-    book.title = title || book.title;
-    book.author = author || book.author;
-    res.send(book);
-});
+//     const { title, author } = req.body;
+//     book.title = title || book.title;
+//     book.author = author || book.author;
+//     res.send(book);
+// });
 
 
-app.delete('/books/:id',(req,res) => {
-    const bookIndex = books.findIndex(b => b.id === parseInt(req.params.id));
-    if (bookIndex === -1){
-        return res.status(404).send('book not found');
-    }
+// app.delete('/books/:id',(req,res) => {
+//     const bookIndex = books.findIndex(b => b.id === parseInt(req.params.id));
+//     if (bookIndex === -1){
+//         return res.status(404).send('book not found');
+//     }
 
-    books.splice(bookIndex, 1);
-    res.status(204).send();
-});
+//     books.splice(bookIndex, 1);
+//     res.status(204).send();
+// });
 
 
 
